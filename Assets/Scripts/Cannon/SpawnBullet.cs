424db5e4784a38e16459fb79fe9f8e32e68968f8
Asoji <asoji-git@hiitsdevin.mozmail.com>
2023-03-26 19:51:20 -0700

    🐛 `NullReferenceException` did your father

--- Assets/Scripts/Cannon/SpawnBullet.cs
+++ Assets/Scripts/Cannon/SpawnBullet.cs
@@ -16,12 +16,13 @@ public class SpawnBullet : MonoBehaviour
         }
         
         // detect if bullet is touching edge of camera
-        if (GameObject.Find("Bullet(Clone)").transform.position.x > 0 ||
-            GameObject.Find("Bullet(Clone)").transform.position.x > Screen.width ||
-            GameObject.Find("Bullet(Clone)").transform.position.y > 0 ||
-            GameObject.Find("Bullet(Clone)").transform.position.y > Screen.height) {
-            Delete();
-        }
+        // if (GameObject.Find("Bullet(Clone)").transform.position.x > 0 ||
+        //     GameObject.Find("Bullet(Clone)").transform.position.x > Screen.width ||
+        //     GameObject.Find("Bullet(Clone)").transform.position.y > 0 ||
+        //     GameObject.Find("Bullet(Clone)").transform.position.y > Screen.height) {
+        //     Delete();
+        // }
+        // so the console is being spammed with `NullReferenceException` errors before a bullet even spawns, time to kill this for now!
     }
 
     void Shoot() {
