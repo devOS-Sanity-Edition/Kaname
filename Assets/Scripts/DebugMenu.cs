b13a554fd462d7a3d83d9b54dab29e20d434d71b
Asoji <asoji-git@hiitsdevin.mozmail.com>
2023-03-28 04:25:16 -0700

    💄 Little more UI stuff, and DebugView checks

--- Assets/Scripts/DebugMenu.cs
+++ Assets/Scripts/DebugMenu.cs
@@ -6,6 +6,9 @@ public class DebugMenu : MonoBehaviour {
     bool m_WindowEnabled = false;
     private float fpsCount;
     public GameObject cannon;
+    bool debugBuild;
+    bool editorBuild;
+    bool displayDebug; // todo: make this a toggle. make debug view not show by default in prod, but show by default in editor and dev/debug builds
     
     private IEnumerator Start()
     {
@@ -24,8 +27,7 @@ public class DebugMenu : MonoBehaviour {
         ImGuiUn.Layout -= OnLayout;
     }
 
-    void ShowDebugView() {
-        // ProfilerRecorder gfxTime = ProfilerRecorder.StartNew(ProfilerCategory.Render, "Gfx.PresentFrame");
+    void ShowDebugView() { 
         if (!ImGui.Begin("Hikaru Debug",
                 ref m_WindowEnabled,
                 ImGuiWindowFlags.NoMove))
@@ -33,6 +35,22 @@ public class DebugMenu : MonoBehaviour {
         
         ImGui.Text($"FPS: {Mathf.Round(fpsCount)}");
         ImGui.Text($"Screen Size: {Screen.width} x {Screen.height}");
+
+        if (ImGui.CollapsingHeader("Build")) {
+            #if UNITY_EDITOR
+                editorBuild = true;
+            #endif
+            ImGui.Text($"Editor Mode: {editorBuild.ToString()}");
+            #if DEVELOPMENT_BUILD // IDES ARE GOING TO SAY THIS IS UNREACHABLE CODE! FALSE! THIS ACTUALLY DOES WORK IN DEVELOPMENT BUILDS BUT OF COURSE
+                                  //
+                                  // +
+                                  //
+                                  // 0+ +0 -
+                                  // 0+  IT DOESNT KNOW THAT AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                debugBuild = true;
+            #endif
+            ImGui.Text($"Debug Build: {debugBuild.ToString()}");
+        }
         
         if (ImGui.CollapsingHeader("Game")) {
             ImGui.Text($"Bullet Count: {bulletCount("Bullet")}");
@@ -44,8 +62,11 @@ public class DebugMenu : MonoBehaviour {
             ImGui.Text($"Timescale Speed: {Time.timeScale.ToString("#0.##%")}%");
             ImGui.Text($"Fixed DeltaTime: {Time.fixedDeltaTime}");
             ImGui.Text($"Tickrate: {Time.fixedDeltaTime * 10000}");
+            // todo: have GPU and CPU timing showing somehow
+            // ProfilerRecorder gfxTime = ProfilerRecorder.StartNew(ProfilerCategory.Render, "Gfx.PresentFrame");
+
         }
-        
+
         ImGui.SetWindowPos(new Vector2(10, 120));
         ImGui.SetWindowSize(new Vector2(300, 400));
         
