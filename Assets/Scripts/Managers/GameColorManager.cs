1717b43eb9571a6ff99a6342043291118da9eb61
Asoji <asoji-git@hiitsdevin.mozmail.com>
2023-04-04 23:58:56 -0700

    🔥 Removed chunk of unused variables

--- Assets/Scripts/Managers/GameColorManager.cs
+++ Assets/Scripts/Managers/GameColorManager.cs
@@ -3,20 +3,6 @@ using System.IO;
 using UnityEngine;
 
 public class GameColorManager : MonoBehaviour {
-    Color gameBackgroundColor;
-    Color gameBackgroundCircleColor;
-    Color gameBulletColor;
-    Color gameCannonColor;
-    Color gameDeathFlashColor;
-    Color gameObstacleLayer1Color;
-    Color gameObstacleLayer2Color;
-    Color gameObstacleLayer3Color;
-    Color gameObstacleLayer4Color;
-    Color uiBackgroundColor;
-    Color uiLabelColor;
-    Color uiLabelNumericColor;
-
-
     string developmentThemeJSON = $"{Application.streamingAssetsPath}/GameData/Themes/Default/Development/theme.json";
 
     [Serializable]
