b04c80ad5b0fcaccd8ef19ff0deb96670c01d22d
Asoji <asoji-git@hiitsdevin.mozmail.com>
2023-04-04 18:42:21 -0700

    🚀 Start some work on DBOnRing
    Co-Authored-By: Ziku <69371519+zikuuuu@users.noreply.github.com>

---
+++ Assets/Scripts/Enemy/DBOnRing.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using System = FMOD.System;
+
+public class DBOnRing : MonoBehaviour {
+    public int DeathBallAmount = 3;
+    
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
