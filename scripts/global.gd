e93c57ec0d331f80587d4490c9e12db55f7ea1cb
Asoji <asoji-git@hiitsdevin.mozmail.com>
2023-09-26 06:47:23 -0700

    ✨ Added quit input

--- scripts/global.gd
+++ scripts/global.gd
@@ -20,6 +20,10 @@ func _ready():
 	pass # Replace with function body.
 	
 
+func _input(event):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().quit()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
